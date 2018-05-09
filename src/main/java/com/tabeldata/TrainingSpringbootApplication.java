bf75031d9316539f13a1917527b675adfb8f31de
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2018-05-09 11:46:35 +0700

    init project springboot

---
+++ src/main/java/com/tabeldata/TrainingSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.tabeldata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingSpringbootApplication.class, args);
+    }
+}
